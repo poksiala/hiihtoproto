75336d4d2d379ab7896a445bde9d1b29254647ab
Panu Oksiala <panu.oksiala@aalto.fi>
2018-11-28 13:31:30 +0200

    test

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const rpio = require('rpio')
 
 const BIOPIN = 7
+const MIXEDPIN = 8
+const PIN_NAMES = {7: 'bio', 8: 'mixed'}
+const PINS = [BIOPIN, MIXEDPIN]
 
-rpio.open(BIOPIN, rpio.INPUT)
-setInterval(() => {
-    console.log(`Pin 4 state is: ${rpio.read(BIOPIN)}`)
-}, 1000)
+
+const handlePinChange = (pin) => {
+    const state = (rpio.read(pin) === 1)
+    const name = PIN_NAMES[pin]
+    console.log(name, state)  
+}
+
+PINS.forEach((id) => {
+  rpio.open(id, rpio.INPUT)
+  rpio.poll(id, handlePinChange)
